8cec6864db5ac2d55ee918df4db5eebdf17a8d30
thPhonG-oss <ngthanhphong0817@gmail.com>
2025-12-08 01:12:55 +0700

    feat: initialize login UI

---
+++ FE/SalesManagement.WinUI/Models/UserResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesManagement.WinUI.Models
+{
+    public class UserResponseDTO
+    {
+        public long Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string AccessToken { get; set; } = string.Empty;
+    }
+}
