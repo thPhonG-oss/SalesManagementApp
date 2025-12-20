10c66b4c312aec522479d74460dcce841a71eb47
Cong Chau <congchau206#gmail.com>
2025-12-21 00:28:09 +0700

    ui page product management, connect api

---
+++ FE/SalesManagement.WinUI/ViewModels/ProductViewModel.cs
@@ -0,0 +1,42 @@
+﻿using SalesManagement.WinUI.Models;
+using SalesManagement.WinUI.Services.Interfaces;
+using System.Collections.ObjectModel;
+
+namespace SalesManagement.WinUI.ViewModels
+{
+    public class ProductViewModel : BaseViewModel
+
+    {
+        private readonly ICategoryService _categoryService;
+
+        public ObservableCollection<Category> Categories { get; }
+            = new ObservableCollection<Category>();
+
+        private Category? _selectedCategory;
+        public Category? SelectedCategory
+        {
+            get => _selectedCategory;
+            set => SetProperty(ref _selectedCategory, value);
+        }
+
+        public ProductViewModel(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+            _ = LoadCategoriesAsync();
+        }
+
+        private async Task LoadCategoriesAsync()
+        {
+            var data = await _categoryService.GetAllAsync();
+
+
+            System.Diagnostics.Debug.WriteLine($"[VM] Categories = {data.Count}");
+
+            Categories.Clear();
+            foreach (var item in data)
+            {
+                Categories.Add(item);
+            }
+        }
+    }
+}
