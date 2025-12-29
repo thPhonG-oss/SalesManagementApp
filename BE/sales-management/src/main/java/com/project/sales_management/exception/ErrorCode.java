2dfbf9119db239dc1a466c0227cf1ad9de67550c
Do Ngoc Cuong <cuong01656499081@gmail.com>
2025-12-29 19:54:41 +0700

    fix usedCount and check promotion successfully

--- BE/sales-management/src/main/java/com/project/sales_management/exception/ErrorCode.java
+++ BE/sales-management/src/main/java/com/project/sales_management/exception/ErrorCode.java
@@ -24,7 +24,8 @@ public enum ErrorCode {
     PHONE_EXIST("1008", "Phone already exists", HttpStatus.BAD_REQUEST),
     CUSTOMER_NOT_FOUND("1009", "CUSTOMER_NOT_FOUND", HttpStatus.BAD_REQUEST),
     ORDER_NOT_FOUND("1010", "ORDER_NOT_FOUND", HttpStatus.BAD_REQUEST),
-    OUT_OF_STOCK("1011","Current inventory is insufficient to fulfill your order.",HttpStatus.BAD_REQUEST)
+    OUT_OF_STOCK("1011","Current inventory is insufficient to fulfill your order.",HttpStatus.BAD_REQUEST),
+    DISCOUNT_NOT_SUITABLE("1012","The discount is not suitable for you.",HttpStatus.BAD_REQUEST)
 
     ;
     ErrorCode(String code, String message, HttpStatus httpStatusCode) {
